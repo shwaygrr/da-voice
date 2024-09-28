4e1f37712dbfd7e38acf43e062fa31d33e574d97
Gabriel Suoth <gabrielsuoth@gmail.com>
2024-09-28 15:55:56 -0400

    election page and info

--- da-voice-frontend/src/routes/Election.jsx
+++ da-voice-frontend/src/routes/Election.jsx
@@ -1,11 +1,64 @@
 import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 
 const Election = () => {
-  useEffect(() => {}, []);
+  const { id } = useParams();
+  const [election, setElection] = useState(null);
+  useEffect(() => {
+    const API_KEY = import.meta.env.VITE_CIVIC_API_KEY;
+    const address = "1930 SW 145th Ave, Miramar, FL 33027";
+    const URL =
+      "https://civicinfo.googleapis.com/civicinfo/v2/voterinfo?address=" +
+      address +
+      "&electionId=" +
+      id +
+      "&productionDataOnly=true&returnAllAvailableData=true&key=" +
+      API_KEY;
+
+    const fetchData = async () => {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      setElection(data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <h1>Test</h1>
+      {election ? (
+        <div>
+          <Link to="/elections" className="back">
+            Back
+          </Link>
+          <h1>{election.election.name}</h1>
+          <h2>{election.election.electionDay}</h2>
+          <h2>Important Links</h2>
+          <a
+            href={
+              election.state[0].electionAdministrationBody
+                .electionRegistrationUrl
+            }
+          >
+            <p>Register Here</p>
+          </a>
+
+          <a
+            href={
+              election.state[0].electionAdministrationBody
+                .votingLocationFinderUrl
+            }
+          >
+            <p>Voting Locations</p>
+          </a>
+
+          <a href={election.state[0].electionAdministrationBody.ballotInfoUrl}>
+            <p>Ballot Info</p>
+          </a>
+        </div>
+      ) : null}
     </div>
   );
 };
