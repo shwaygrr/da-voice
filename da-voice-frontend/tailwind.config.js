d05be1a3c3fce0999e48fca43596e5282e4330a8
Chwoeger Charleston <122495108+shwaygrr@users.noreply.github.com>
2024-09-28 13:26:14 -0400

    init tailwind

---
+++ da-voice-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Add other file types if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
