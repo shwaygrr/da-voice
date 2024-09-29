1a2a59b5f30f215188cb13d69421ad8fddd6e397
Gabriel Suoth <gabrielsuoth@gmail.com>
2024-09-28 21:29:47 -0400

    rep articles

--- da-voice-frontend/src/routes/Representative.jsx
+++ da-voice-frontend/src/routes/Representative.jsx
@@ -1,11 +1,12 @@
 import { useLocation, Link } from "react-router-dom";
 import { IoMdArrowRoundBack } from "react-icons/io";
-
+import InfoArticle from "../components/InfoArticle";
+import { useEffect, useState } from "react";
 
 const Representative = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-
+  const [articles, setArticles] = useState(null);
   const params = {
     name: queryParams.get("name"),
     position: queryParams.get("position"),
@@ -15,10 +16,40 @@ const Representative = () => {
     address: queryParams.get("address"),
   };
 
+  useEffect(() => {
+    const API_KEY = import.meta.env.VITE_FASTAPI_KEY;
+
+    const URL =
+      "https://real-time-news-data.p.rapidapi.com/search?query=" +
+      params.name +
+      "&limit=5&time_published=anytime&country=US&lang=en";
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": API_KEY,
+        "x-rapidapi-host": "real-time-news-data.p.rapidapi.com",
+      },
+    };
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL, options);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setArticles(data.data);
+      } catch (error) {
+        console.error("Failed to fetch articles:", error);
+      }
+    };
+    fetchData();
+  }, [params.name]);
+
   return (
     <div className="repres">
       <Link to="/" className="back">
-        <IoMdArrowRoundBack className="text-xl"/>
+        <IoMdArrowRoundBack className="text-xl" />
       </Link>
       <h1 className="text-3xl font-bold mb-4 pt-2">{params.name}</h1>
       <h2 className="text-lg mb-2">{params.position}</h2>
@@ -30,6 +61,22 @@ const Representative = () => {
       <p className="mt-2">{params.phone}</p>
       <h2 className="text-lg font-semibold mt-4">Address</h2>
       <p>{params.address}</p>
+      <div className="mt-4">
+        <h1 className="text-3xl font-bold mb-4 pt-2">Articles</h1>
+        {articles ? (
+          articles.map((article, index) => (
+            <InfoArticle
+              key={index}
+              url={article.link}
+              image={article.photo_url}
+              headline={article.title}
+              content={article.snippet}
+            />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 };
